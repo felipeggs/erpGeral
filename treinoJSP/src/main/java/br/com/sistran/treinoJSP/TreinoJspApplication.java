395a4b0853231b8f359df08279b082bfc4843be7
Felipe Gabriel Gervazio Sotero <73715646+felipeggs@users.noreply.github.com>
2023-08-23 11:43:44 -0300

    Add files via upload

---
+++ treinoJSP/src/main/java/br/com/sistran/treinoJSP/TreinoJspApplication.java
@@ -0,0 +1,13 @@
+package br.com.sistran.treinoJSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoJspApplication.class, args);
+	}
+
+}
---
+++ treinoJSP/src/test/java/br/com/sistran/treinoJSP/TreinoJspApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistran.treinoJSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinoJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
